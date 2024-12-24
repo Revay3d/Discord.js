867023342e763b8bcde980982e7289cd6960c5ab
Yasu Yumiko <yumikokun0828@gmail.com>
2024-12-24 11:31:45 -0500

    refactor: config file

--- utils/createCommands.js
+++ utils/createCommands.js
@@ -1,24 +1,24 @@
-const fs = require("fs");
-const { REST, Routes } = require("discord.js");
-const config = require("../src/config.json");
-require("dotenv").config();
-const commands = [];
-const slashCommandsFiles = fs
-  .readdirSync("./src/commands/slashCommands")
-  .filter((file) => file.endsWith("js"));
+import { readdirSync } from "node:fs";
+import { REST, Routes } from "discord.js";
+import { clientId, guildId } from "../src/config.js";
 
-for (const file of slashCommandsFiles) {
-  const slash = require(`../src/commands/slashCommands/${file}`);
-  commands.push(slash.data.toJSON());
-}
+const commands = [];
+const slashCommandsFiles = readdirSync("./src/commands/slashCommands").filter(
+  (file) => file.endsWith("js")
+);
 
 const rest = new REST({ version: "10" }).setToken(process.env.token);
 
-createSlash();
-
 async function createSlash() {
   try {
-    await rest.put(Routes.applicationCommands(config.clientId, config.guildId), {
+    for (const file of slashCommandsFiles) {
+      const { default: slash } = await import(
+        `../src/commands/slashCommands/${file}`
+      );
+      commands.push(slash.data.toJSON());
+    }
+
+    await rest.put(Routes.applicationCommands(clientId, guildId), {
       body: commands,
     });
     console.log("[Slash Commands] Agregados.");
@@ -26,3 +26,5 @@ async function createSlash() {
     console.error(e);
   }
 }
+
+createSlash();
